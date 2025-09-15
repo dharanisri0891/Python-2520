3e83541c0a536d919881894c81efb94778793fc9
dharanisri0891 <dharanisri0891@gmail.com>
2025-09-15 07:51:25 +0530

    new

--- basics.py
+++ basics.py
@@ -1,3 +1,28 @@
 a=10
 b=20
 print(a+b)
+
+#Identifiers
+data = "10th septemebr 2025"
+print(data)
+
+#variables
+student_name = "dharani"
+student_age = "25"
+student_cgpa = "8.9"
+
+print(student_name)
+print(student_age)
+print(student_cgpa)
+
+# get identity/ memory
+
+print(id(student_name))
+print(id(student_age))
+print(id(student_cgpa))
+
+
+#Get type of the data
+print(type(student_name))
+print(type(student_age))
+print(type(student_cgpa))
